12d6119cd63ffb18cb27054291d5f4a17328ebef
Marcus Nerløe <marcusnerloe@gmail.com>
2021-02-22 23:06:02 +0100

    Linux fixes

--- scraping.ts
+++ scraping.ts
@@ -5,7 +5,6 @@ export async function IsPhoneInStock(phone: PhoneStock) {
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: { width: 1920, height: 1080 },
-    args: ["--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
 
